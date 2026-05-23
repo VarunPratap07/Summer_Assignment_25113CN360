fc779dc7fef07bee49208844ab8f2b3191ceb74f
VarunPratap07 <varunpratapsinghshishodia@gmail.com>
2026-05-23 20:51:23 +0530

    Q2 attempt

---
+++ Day2/Q2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int n ,n2, digits=0 , digit , revnum=0 ;
+    printf("Please enter the number of which you want The Number of digit of of = ");
+    scanf("%d",&n);
+    //Since n is lost after loop
+    n2=n;
+    while(n>0){
+        n=n/10;
+        digits+=1;
+    }
+    printf("%d\n",digits);
+    n=n2;
+    while(n>0){
+        digit=n%10;
+        revnum+=digit*(pow(10,digits-1));
+        digits-=1;
+        n=n/10;
+    }
+    printf("The reverse of %d is %d \n",n2,revnum);
+    return 0;
+}
